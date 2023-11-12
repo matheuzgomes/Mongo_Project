11dfbca0ca268115784fa80e156300efcc66c75c
Marinho <matmarinho2009@hotmail.com>
2023-11-12 01:01:14 -0300

    adding cbv to controllers

--- src/controllers/task_controller.py
+++ src/controllers/task_controller.py
@@ -1,40 +1,45 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
+from fastapi_utils.cbv import cbv
+from fastapi_utils.inferring_router import InferringRouter
 from ..services.insert_task import InsertTasksService
 from ..services.get_all_tasks import GetAllTasksService
 from ..infra.repositories import TaskRepository
 from ..services.DTOs import InsertTaskRequest
 
 task_route = APIRouter()
+task_router = InferringRouter()
+
+@cbv(task_router)
+class Taskontroller():
+    @task_route.post("/")
+    def insert_task_controller(data: InsertTaskRequest):
+
+        insert_data = InsertTaskRequest(task_name=data.task_name,
+                                    task_status=data.task_status,
+                                    description=data.description)
+        
+
+        try:
+            data = InsertTasksService.insert_tasks(TaskRepository(), data=insert_data)
 
 
-@task_route.post("/")
-def insert_task_controller(data: InsertTaskRequest):
+        except Exception as error:
+            raise Exception(error)
 
-    insert_data = InsertTaskRequest(task_name=data.task_name,
-                                   task_status=data.task_status,
-                                   description=data.description)
-    
-
-    try:
-        data = InsertTasksService.insert_tasks(TaskRepository(), data=insert_data)
-
-
-    except Exception as error:
-        raise Exception(error)
-
-    return JSONResponse(content=jsonable_encoder(data.__dict__))
+        return JSONResponse(content=jsonable_encoder(data.__dict__))
 
 
 
-@task_route.get("/list")
-def get_all_tasks_controller():
-    try:
-        data = GetAllTasksService.get_all(TaskRepository())
+    @task_route.get("/list")
+    def get_all_tasks_controller():
+        try:
+            data = GetAllTasksService.get_all(TaskRepository())
 
-    except Exception as error:
-        raise Exception(error) from error
-    
-    return JSONResponse(content=jsonable_encoder(data.__dict__))
+        except Exception as error:
+            raise Exception(error) from error
+        
+        return JSONResponse(content=jsonable_encoder(data.__dict__))
 
+task_route.include_router(task_router)
