b55f3809e066d3d5101f18b8490c2667d4daaaab
Marinho <matmarinho2009@hotmail.com>
2024-02-28 22:56:40 -0300

    wrapper assincrono para funcao assincrona

--- src/controllers/utils/benchmark_wrapper.py
+++ src/controllers/utils/benchmark_wrapper.py
@@ -5,9 +5,9 @@ import time
 def benchmark(func):
     '''Benchmark My Controller'''
     @wraps(func)
-    def wrapper(*args, **kwargs):
+    async def wrapper(*args, **kwargs):
         start = time.perf_counter()
-        result = func(*args, **kwargs)
+        result = await func(*args, **kwargs)
         end = time.perf_counter()
         response = end - start
         print(f"Time elapsed in {func.__name__} was {response:.4f} seconds")
