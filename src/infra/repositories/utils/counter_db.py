ea6fcbfaf43b02cf42eee350c7ca8cde5f8d0d91
Marinho <matmarinho2009@hotmail.com>
2024-03-13 23:14:59 -0300

    fix

--- src/infra/repositories/utils/counter_db.py
+++ src/infra/repositories/utils/counter_db.py
@@ -4,7 +4,20 @@ class CounterDB:
 
     @staticmethod
     def counter(db) -> None:
-        connection = db
-        id = connection.find_one(sort=[('_id', DESCENDING)])
-        counter = connection.insert_one({"_id": id['_id'] + 1})
+
+        try:
+
+            connection = db
+            id = connection.find_one(sort=[('_id', DESCENDING)])
+            counter = connection.insert_one({"_id": id['_id'] + 1})
+
+        except Exception as e:
+
+            if isinstance(e, TypeError):
+                connection.insert_one({"_id": 0})
+                retry_id = connection.find_one(sort=[('_id', DESCENDING)])
+                counter = connection.insert_one({"_id": retry_id['_id'] + 1})
+
+                return counter
+   
         return counter
